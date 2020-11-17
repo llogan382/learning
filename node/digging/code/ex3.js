c4bb5b0106cf4c543d7884e2cf69a60846e01d3d
Luke <llogan382@gmail.com>
2020-11-17 09:33:44 -0500

    Updates both commits

--- node/digging/code/ex3.js
+++ node/digging/code/ex3.js
@@ -7,12 +7,24 @@ var fs = require("fs");
 var Transform = require("stream").Transform;
 var zlib = require("zlib");
 
+
+var CAF = require("caf");
+
 // var getStdin = require("get-stdin");
 
 var args = require("minimist")(process.argv.slice(2), {
     boolean: ["help", "in", "out", "compress", "uncompress"],
     string: ["file"]
 });
+
+processFile = CAF(processFile);
+
+function streamComplete(stream){
+    return new Promise(function c(res){
+        stream.on("end",res);
+    });
+}
+
 
 var BASE_PATH = path.resolve(
     process.env.BASE_PATH || __dirname
@@ -26,20 +38,31 @@ if (args.help) {
 else if (args.in ||
     args._.includes("-")
 ) {
-    processFile(process.stdin);
+
+    let tooLong = CAF.timeout(17,"Took Too Long!");
+
+    processFile(tooLong,process.stdin)
+    .catch(error);
 }
 else if (args.file) {
     let stream = fs.createReadStream(path.join(BASE_PATH,args.file));
-    processFile(stream);
 
+    let tooLong = CAF.timeout(17,"Took Too Long!");
 
+    processFile(tooLong,stream)
+    .then(function(){
+
+        console.log("Complete!");
+    })
+    .catch(error);
 }
 else {
     error("incorrect Usage", true);
 };
 
-function processFile(inStream) {
+function *processFile(signal,inStream) {
     var outStream = inStream;
+
 
     if(args.uncompress){
         let gunzipStream = zlib.createGunzip();
@@ -50,9 +73,7 @@ function processFile(inStream) {
     var upperStream = new Transform({
         transform(chunk,enc,cb){
             this.push(chunk.toString().toUpperCase());
-
             cb();
-
         }
     });
 
@@ -72,10 +93,16 @@ function processFile(inStream) {
         targetStream = fs.createWriteStream(OUTFILE);
     }
 
-    process.stdout;
+
+
     outStream.pipe(targetStream);
 
+    signal.pr.catch(function f(){
+        outStream.unpipe(targetStream);
+        outStream.destroy();
+    });
 
+    yield streamComplete(outStream);
 };
 // console.log(contents);
 
@@ -93,10 +120,10 @@ function error(msg, includeHelp = false) {
 
 
 function printHelp() {
-    console.log("ex1 usage:");
-    console.log("  ex1.js --file={FILENAME");
+    console.log("ex3 usage:");
+    console.log("  ex3.js --file={FILENAME");
     console.log("");
-    console.log(" ex1.js --help");
+    console.log(" ex3.js --help");
     console.log("");
     console.log("--help             print this help");
     console.log("--file={FILENAME}     priocess the file");
