cdaeab525cdfbb081db9a45c8c83b5c29ac90f02
Luke <llogan382@gmail.com>
2020-11-17 13:21:00 -0500

    Child processes in node

--- node/digging/code/ex6.js
+++ node/digging/code/ex6.js
@@ -6,15 +6,15 @@ var util = require("util");
 var path = require("path");
 var http = require("http");
 
+var express = require("express");
 var sqlite3 = require("sqlite3");
-var staticAlias = require("node-static-alias");
 
 
 // ************************************
 
 const DB_PATH = path.join(__dirname,"my.db");
 const WEB_PATH = path.join(__dirname,"web");
-const HTTP_PORT = 8032;
+const HTTP_PORT = 8037;
 
 var delay = util.promisify(setTimeout);
 
@@ -35,34 +35,8 @@ var SQL3 = {
 	exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
-var fileServer = new staticAlias.Server(WEB_PATH,{
-	cache: 100,
-	serverInfo: "Node Workshop: ex5",
-	alias: [
-		{
-			match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
-			serve: "index.html",
-			force: true,
-		},
-		{
-			match: /^\/js\/.+$/,
-			serve: "<% absPath %>",
-			force: true,
-		},
-		{
-			match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
-			serve: function onMatch(params) {
-				return `${params.basename}.html`;
-			},
-		},
-		{
-			match: /[^]/,
-			serve: "404.html",
-		},
-	],
-});
+var httpserv = http.createServer(app);
 
-var httpserv = http.createServer(handleRequest);
 
 main();
 
@@ -70,33 +44,71 @@ main();
 // ************************************
 
 function main() {
+	// TODO: define routes
+	//
+	// Hints:
+	//
+	// {
+	// 	match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
+	// 	serve: "index.html",
+	// 	force: true,
+	// },
+	// {
+	// 	match: /^\/js\/.+$/,
+	// 	serve: "<% absPath %>",
+	// 	force: true,
+	// },
+	// {
+	// 	match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
+	// 	serve: function onMatch(params) {
+	// 		return `${params.basename}.html`;
+	// 	},
+	// },
+	// {
+	// 	match: /[^]/,
+	// 	serve: "404.html",
+	// },
+    defineRoutes();
 	httpserv.listen(HTTP_PORT);
-
 	console.log(`Listening on http://localhost:${HTTP_PORT}...`);
-
-
 }
 
-async function handleRequest(req,res){
 
-	if (req.url == "/get-records"){
-		let records = await getAllRecords();
-
-		await delay(1000);
-
+function defineRoutes(){
+    // Setup middleware
+    app.get("/get-records",async function(req,res){
+        var records =await getAllRecords();
 		res.writeHead(200,{"Content-type": "application/json",
 		"Cache-Control": "no-cache"
-
-
-	});
+    });
 	res.end(JSON.stringify(records));
-	}
-	else {
-		fileServer.serve(req,res);
 
-	}
+});
 
-}
+app.use(function(req,res,next){
+    if (/^\/(?:index\/?)?(?:[?#].*$)?$/.test(req.url)) {
+        req.url = "/index.html";
+    }
+    else if (/^\/js\/.+$/.test(req.url)) {
+        next();
+        return;
+    }
+    else if (/^\/(?:[\w\d]+)(?:[\/?#].*$)?$/.test(req.url)) {
+        let [,basename] = req.url.match(/^\/([\w\d]+)(?:[\/?#].*$)?$/);
+        req.url = `${basename}.html`;
+    }
+
+    next();
+})
+
+app.use(express.static(WEB_PATH)),{
+    maxAge: 100,
+    setHeaders: function setHeaders(res){
+        res.setHeader("Server", "Node Workshop: Ex6");
+    }
+};
+
+
 
 // *************************
 // NOTE: if sqlite3 is not working for you,
